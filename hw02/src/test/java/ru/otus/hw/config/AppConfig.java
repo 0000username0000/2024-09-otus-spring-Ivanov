67b4a07c01951fe2e02c3ed3465aa253b4bcf555
Artem <0aaivanov0@gmail.com>
2024-10-22 12:33:37 +0300

    Добавит тест CsvQuestionDao

---
+++ hw02/src/test/java/ru/otus/hw/config/AppConfig.java
@@ -0,0 +1,10 @@
+package ru.otus.hw.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@PropertySource("applicationTest.properties")
+@Configuration
+public class AppConfig {
+
+}
---
+++ hw02/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -0,0 +1,35 @@
+package ru.otus.hw.dao;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.domain.Question;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+
+class CsvQuestionDaoTest {
+
+    private CsvQuestionDao csvQuestionDao;
+
+    private TestFileNameProvider testFileNameProvider;
+
+    @BeforeEach
+    void setUp() {
+        testFileNameProvider = spy(TestFileNameProvider.class);
+        csvQuestionDao = new CsvQuestionDao(testFileNameProvider);
+    }
+
+    @DisplayName("Should find all question from csv file")
+    @Test
+    void testFindAllReturnsQuestionsFromCsv() {
+        when(testFileNameProvider.getTestFileName()).thenReturn("questionsTest.csv");
+        List<Question> questions = csvQuestionDao.findAll();
+        Assertions.assertNotEquals(Collections.emptyList(), questions);
+    }
+}
