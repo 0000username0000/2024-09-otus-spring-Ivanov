11585849dacf1eeba870f1c4a9dd18122f2066a4
0000username0000 <0aaivanov@gmail.com>
2024-10-29 23:43:28 +0300

    Добавит spring shell в pom, app.yml в test

--- hw04/src/main/java/ru/otus/hw/Application.java
+++ hw04/src/main/java/ru/otus/hw/Application.java
@@ -3,7 +3,9 @@ package ru.otus.hw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.shell.command.annotation.CommandScan;
 
+@CommandScan
 @PropertySource("application.yml")
 @SpringBootApplication
 public class Application {
