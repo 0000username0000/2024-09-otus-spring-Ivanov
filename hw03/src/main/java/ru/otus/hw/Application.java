066fe142ec4c0ef117b263c47749da99bdefc3c7
Artem <0aaivanov0@gmail.com>
2024-10-29 16:30:36 +0300

    Добавит вопросы, добавит тесты, добавит баннер

--- hw03/src/main/java/ru/otus/hw/Application.java
+++ hw03/src/main/java/ru/otus/hw/Application.java
@@ -2,15 +2,16 @@ package ru.otus.hw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import ru.otus.hw.config.AppProperties;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import ru.otus.hw.service.TestRunnerService;
 
-
-@EnableConfigurationProperties(AppProperties.class)
+@PropertySource("application.yml")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
---
+++ hw03/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -0,0 +1,45 @@
+package ru.otus.hw.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.domain.TestResult;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.CsvQuestionDao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+
+public class TestServiceImplTest {
+
+    private TestServiceImpl testService;
+    private LocalizedIOService ioService;
+    private TestFileNameProvider fileNameProvider;
+
+    @BeforeEach
+    public void setUp() {
+        ioService = Mockito.mock(LocalizedIOService.class);
+        fileNameProvider = Mockito.mock(TestFileNameProvider.class);
+        QuestionDao questionDao = new CsvQuestionDao(fileNameProvider);
+        testService = new TestServiceImpl(ioService, questionDao);
+    }
+
+    @Test
+    public void testExecuteTestFor() {
+        when(fileNameProvider.getTestFileName()).thenReturn("questionsTest.csv");
+        when(ioService.readIntForRangeWithPrompt(anyInt(), anyInt(), anyString(), anyString()))
+                .thenReturn(1, 1, 3, 1, 4);
+
+        Student student = new Student("Name","Surname");
+
+        TestResult result = testService.executeTestFor(student);
+
+        assertThat(result.getStudent()).isEqualTo(student);
+        assertThat(result.getRightAnswersCount()).isEqualTo(5);
+    }
+}
