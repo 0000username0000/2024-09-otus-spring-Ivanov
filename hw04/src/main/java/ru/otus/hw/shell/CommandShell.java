362ac70d9708d90bc59943e996a1109bc69fa929
0000username0000 <0aaivanov@gmail.com>
2024-11-01 21:16:20 +0300

    fix

--- hw04/src/main/java/ru/otus/hw/shell/CommandShell.java
+++ hw04/src/main/java/ru/otus/hw/shell/CommandShell.java
@@ -2,20 +2,10 @@ package ru.otus.hw.shell;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
-import org.springframework.shell.standard.ShellMethodAvailability;
-import ru.otus.hw.dao.QuestionDao;
-import ru.otus.hw.domain.Question;
-import ru.otus.hw.domain.Student;
-import ru.otus.hw.domain.TestResult;
-import ru.otus.hw.service.ResultService;
-import ru.otus.hw.service.StreamsIOService;
 import ru.otus.hw.service.TestRunnerService;
 
-import java.util.List;
-import java.util.Objects;
 
 @ShellComponent(value = "Application Events Command")
 @RequiredArgsConstructor
