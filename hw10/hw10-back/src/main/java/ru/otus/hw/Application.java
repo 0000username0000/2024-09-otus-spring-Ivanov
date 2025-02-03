c459e97dd85908ec8c6a97508988560d64a504bb
0000username0000 <romperstompersd@gmail.com>
2025-02-03 14:49:00 +0300

    fix

--- hw10/hw10-back/src/main/java/ru/otus/hw/Application.java
+++ hw10/hw10-back/src/main/java/ru/otus/hw/Application.java
@@ -8,7 +8,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.printf("%n%s%nLink:%n%s%n", "Enter in console: nmp start", "http://localhost:3000");
+		System.out.printf("%n%s%nLink:%n%s%n", "Enter in console: npm install, npm start", "http://localhost:3000");
 	}
 
 }
