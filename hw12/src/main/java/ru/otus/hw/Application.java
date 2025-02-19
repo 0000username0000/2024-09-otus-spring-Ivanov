79fe8991311ff4dc27952028d7d6c0a92326ca36
0000username0000 <romperstompersd@gmail.com>
2025-02-19 18:01:46 +0300

    Исправит замечание

--- hw12/src/main/java/ru/otus/hw/Application.java
+++ hw12/src/main/java/ru/otus/hw/Application.java
@@ -9,10 +9,15 @@ import java.sql.SQLException;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.printf("Link: %n%s%n", "http://localhost:8080");
-		Console.main(args);
-	}
+        try {
+            Console.main(args);
+        } catch (SQLException e) {
+			System.err.println("Start application error: " + e.getMessage());
+			e.printStackTrace();
+        }
+    }
 
 }
