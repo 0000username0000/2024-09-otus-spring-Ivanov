47428f4bf18a466e2de6066406eedb0b950d1c12
0000username0000 <romperstompersd@gmail.com>
2025-02-25 20:19:53 +0300

    Добавит простой healthIndicator, реализацию spring data rest

--- hw16/src/main/java/ru/otus/hw/repositories/CommentRepository.java
+++ hw16/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -1,12 +1,16 @@
 package ru.otus.hw.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import ru.otus.hw.models.Comment;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "comments_rest")
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    @RestResource(path = "book_comments", rel = "book_comments")
     List<Comment> findByBookId(Long bookId);
 
 }
--- hw16/src/main/java/ru/otus/hw/repositories/GenreRepository.java
+++ hw16/src/main/java/ru/otus/hw/repositories/GenreRepository.java
@@ -1,13 +1,17 @@
 package ru.otus.hw.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import ru.otus.hw.models.Genre;
 
 import java.util.List;
 import java.util.Set;
 
+@RepositoryRestResource(path = "genres_rest")
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 
+    @RestResource(path = "genres", rel = "genres")
     List<Genre> findByIdIn(Set<Long> ids);
 
 }
