3e643c5a13d20b1a9f6633e2b3ed32305d1bbdf7
0000username0000 <romperstompersd@gmail.com>
2025-02-25 20:24:24 +0300

    checkstyle

--- hw16/src/main/java/ru/otus/hw/actuator/RandomHealthIndicator.java
+++ hw16/src/main/java/ru/otus/hw/actuator/RandomHealthIndicator.java
@@ -2,7 +2,6 @@ package ru.otus.hw.actuator;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.boot.actuate.health.Status;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalTime;
