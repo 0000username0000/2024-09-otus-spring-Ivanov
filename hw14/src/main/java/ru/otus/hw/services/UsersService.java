a14963d69f29f7f470793a08807124e58a5852e2
0000username0000 <romperstompersd@gmail.com>
2025-02-26 13:38:37 +0300

    checkstyle

--- hw14/src/main/java/ru/otus/hw/services/UsersService.java
+++ hw14/src/main/java/ru/otus/hw/services/UsersService.java
@@ -2,16 +2,15 @@ package ru.otus.hw.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.models.Users;
 import ru.otus.hw.repositories.UsersRepository;
 
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
